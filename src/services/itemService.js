4a6bb0a8c4c653da824cd7493e3d6a5ef58a15d4
breyshaw <dawsonbrey@gmail.com>
2022-01-20 16:38:45 -0500

    create itemService module

---
+++ src/services/itemService.js
@@ -0,0 +1,16 @@
+import * as tokenService from '../services/tokenService'
+
+const BASE_URL = `/api/items`
+
+function createItem(itemData) {
+  return fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${tokenService.getToken()}`, 'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(itemData)
+  })
+    .then(res => res.json())
+}
+
+export { createItem }
