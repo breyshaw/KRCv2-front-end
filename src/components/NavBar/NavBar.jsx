ca250853ec8efc082dd06d1fe741f3e6b1d3fcd9
breyshaw <dawsonbrey@gmail.com>
2022-02-07 12:41:29 -0500

    fix warning messages in console

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,27 +4,27 @@ const NavBar = ({ user, handleLogout }) => {
   return (
     <>
     {user ? 
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">Katie Rayne Creations</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">Katie Rayne Creations</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/items">All Items</a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/items">All Items</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/clothes">Clothing</a>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/clothes">Clothing</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/tumblers">Tumblers</a>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/tumblers">Tumblers</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/misc">Misc</a>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/misc">Misc</a>
               </li>
-              <li class="nav-item"><Link class="nav-link active" aria-current="page" to="" onClick={handleLogout}>LOG OUT</Link></li>
+              <li className="nav-item"><Link className="nav-link active" aria-current="page" to="" onClick={handleLogout}>LOG OUT</Link></li>
               {user ?
                 (user.isAdmin) &&
                 <Link to="/addItem" className="btn btn-primary">Add Item</Link>
@@ -35,31 +35,31 @@ const NavBar = ({ user, handleLogout }) => {
         </div>
       </nav>
     :
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">Katie Rayne Creations</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">Katie Rayne Creations</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/items">All Items</a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/items">All Items</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/clothes">Clothing</a>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/clothes">Clothing</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/tumblers">Tumblers</a>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/tumblers">Tumblers</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/misc">Misc</a>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/misc">Misc</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/signup">Sign-Up</a>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/signup">Sign-Up</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/login">Login</a>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/login">Login</a>
               </li>
             </ul>
           </div>
