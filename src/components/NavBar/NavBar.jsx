d558c74392b20a37e58d0a1b97e6df7855ab4af5
breyshaw <dawsonbrey@gmail.com>
2022-02-07 12:20:05 -0500

    add Navbar functionality, clean code

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
+    {user ? 
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="/">Katie Rayne Creations</a>
@@ -12,42 +13,54 @@ const NavBar = ({ user, handleLogout }) => {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="/items">All Items</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link active" aria-current="page" href="/clothes">Clothing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/tumblers">Tumblers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/misc">Misc</a>
+              </li>
+              <li class="nav-item"><Link class="nav-link active" aria-current="page" to="" onClick={handleLogout}>LOG OUT</Link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    :
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">Katie Rayne Creations</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/items">All Items</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/clothes">Clothing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/tumblers">Tumblers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/misc">Misc</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/signup">Sign-Up</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/login">Login</a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
-
-      {/* {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
-            <li><Link to="/addItem">Add Item</Link></li>
-            <li><Link to="/items">All Items</Link></li>
-            <li><Link to="/clothes">Clothing</Link></li>
-            <li><Link to="/tumblers">Tumblers</Link></li>
-            <li><Link to="/misc">Misc. Crafts</Link></li>
-          </ul>
-        </nav>
-        :
-        <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-            <li><Link to="/items">All Items</Link></li>
-            <li><Link to="/clothes">Clothing</Link></li>
-            <li><Link to="/tumblers">Tumblers</Link></li>
-            <li><Link to="/misc">Misc. Crafts</Link></li>
-          </ul>
-        </nav>
-      } */}
+  }
     </>
   )
 }
