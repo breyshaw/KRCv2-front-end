3e8f4a186e10b2264376ec5d5238333086d35622
breyshaw <dawsonbrey@gmail.com>
2022-02-07 12:11:13 -0500

    style navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,26 @@ import { Link } from 'react-router-dom'
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
-      {user ?
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">Katie Rayne Creations</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      {/* {user ?
         <nav>
           <ul>
             <li>Welcome, {user.name}</li>
@@ -28,7 +47,7 @@ const NavBar = ({ user, handleLogout }) => {
             <li><Link to="/misc">Misc. Crafts</Link></li>
           </ul>
         </nav>
-      }
+      } */}
     </>
   )
 }
