42da8005cb3714fb065fe68c76c953df16fa95f7
breyshaw <dawsonbrey@gmail.com>
2022-02-08 15:04:34 -0500

    add landing logo

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,11 @@
 import styles from './Landing.module.css'
+import logo from './krc_logo.png'
 
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
       <h1>hello, {user ? user.name : 'friend'}</h1>
+      <img className={styles.logo} src={logo} alt="" />
     </main>
   )
 }
