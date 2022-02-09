7f0dcb0b4ca9808c7bfdb74aa87d2d91791513c8
breyshaw <dawsonbrey@gmail.com>
2022-02-09 13:50:38 -0500

    add aboutme to bootstrap card

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,8 +5,10 @@ const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
       <img className={styles.logo} src={logo} alt="" />
-      <h1>Welcome, {user ? user.name + "!!" : 'friend!!'}</h1>
+      <div className='card'>
+      <h1 className='text-center'>Welcome, {user ? user.name + "!!" : 'friend!!'}</h1>
       <p className={styles.p}>In early 2020, I got the idea to try my hand at custom crafts. I wanted to be able to showcase my different artistic abilities through custom apparel! Since, Katie Rayne Creations has grown into a small, successful business. Not only do I create custom tees, I now offer other items such as tumblers, keychains, bows, blankets, and other accessories! So far, the continued growth and success of KRC has given me the opportunity to make it my full time job, and has allowed me to stay home with my two children. As always, thank you for supporting my small business!</p>
+      </div>
     </main>
   )
 }
