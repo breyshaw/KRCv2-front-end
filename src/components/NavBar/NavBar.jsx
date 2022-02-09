4ab01986b9b634b0a1e2bdf9af6ebfd1163f2ffc
breyshaw <dawsonbrey@gmail.com>
2022-02-09 13:40:36 -0500

    adjust font on Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = ({ user, handleLogout }) => {
       {user ?
         <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light border border-dark bg-gradient opacity-75">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/"><img src={logo} alt="" width="30" height="24" class="d-inline-block align-text-top"/>Katie Rayne Creations</a>
+            <a className="navbar-brand" href="/"><img src={logo} alt="" width="30" height="24" class="d-inline-block align-text-top" />Katie Rayne Creations</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -38,30 +38,32 @@ const NavBar = ({ user, handleLogout }) => {
         :
         <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light border border-dark bg-gradient opacity-75">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/">  
-            <img src={logo} alt="" width="30" height="24" class="d-inline-block align-text-top"/>Katie Rayne Creations</a>
+            <a className="navbar-brand" href="/">
+              <img src={logo} alt="" width="30" height="24" class="d-inline-block align-text-top" />Katie Rayne Creations</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-center">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/items">All Items</a>
+                  <a className="nav-link active" aria-current="page" href="/items">
+                    <h2>All Items</h2></a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/clothes">Clothing</a>
+                  <a className="nav-link active" aria-current="page" href="/clothes">
+                    <h2>Clothing</h2></a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/tumblers">Tumblers</a>
+                  <a className="nav-link active" aria-current="page" href="/tumblers"><h2>Tumblers</h2></a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/misc">Misc</a>
+                  <a className="nav-link active" aria-current="page" href="/misc"><h2>Misc</h2></a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/login">Login</a>
+                  <a className="nav-link active" aria-current="page" href="/login"><h3>Login</h3></a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/signup">Sign-Up</a>
+                  <a className="nav-link active" aria-current="page" href="/signup"><h3>Sign-Up</h3></a>
                 </li>
               </ul>
             </div>
