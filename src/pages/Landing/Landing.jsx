8677e6a5b7ee2633858af35688aa65ada19efab6
breyshaw <dawsonbrey@gmail.com>
2022-02-14 18:26:34 -0500

    add fb page plugin

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -8,6 +8,7 @@ const Landing = ({ user }) => {
       <div className='card'>
         <h1 className='text-center'>Welcome, {user ? user.name + "!!" : 'friend!!'}</h1>
         <p className={styles.p}>In early 2020, I got the idea to try my hand at custom crafts. I wanted to be able to showcase my different artistic abilities through custom apparel! Since, Katie Rayne Creations has grown into a small, successful business. Not only do I create custom tees, I now offer other items such as tumblers, keychains, bows, blankets, and other accessories! So far, the continued growth and success of KRC has given me the opportunity to make it my full time job, and has allowed me to stay home with my two children. <br /> As always, thank you for supporting my small business!</p>
+        <div className="fb-page align-self-center" data-href="https://m.facebook.com/KatieRayneCreations" data-tabs="timeline" data-width="540" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://m.facebook.com/KatieRayneCreations" class="fb-xfbml-parse-ignore"><a href="https://m.facebook.com/KatieRayneCreations">Katie Rayne Creations</a></blockquote></div>  
         {user ? '' :
         <h2 className='text-center'>Want to leave a review on my items? Create an account here! <br /> <a className="btn btn-primary" aria-current="page" href="/signup">Signup</a></h2>
         }
