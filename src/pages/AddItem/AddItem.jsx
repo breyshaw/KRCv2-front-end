f2efd199d3797b9b1fcfeed9957424c76e1ed666
breyshaw <dawsonbrey@gmail.com>
2022-02-14 13:31:12 -0500

    edit addItem form

--- src/pages/AddItem/AddItem.jsx
+++ src/pages/AddItem/AddItem.jsx
@@ -36,53 +36,69 @@ const AddItem = (props) => {
   }
 
   return (
-    <main className={styles.container}>
-
+    <main className='text-center'>
+      <h1 className='mt-4 mb-0'>Post an item, Admin!</h1>
       <form
+        className={styles.container}
         autoComplete="off"
         onSubmit={handleSubmit}
       >
-        <p>Item Name</p>
-        <input type="text"
-          value={title}
-          name="title"
-          onChange={handleChange}
-        />
-        <p>Image Url</p>
-        <input type="text"
-          value={imageUrl}
-          name="imageUrl"
-          onChange={handleChange}
-        />
-        <p>Item Type:</p>
-        <select name="type"
-          value={type}
-          onChange={handleChange}
-        >
-          <option>--Please choose an item type--</option>
-          <option>clothes</option>
-          <option>tumblers</option>
-          <option>misc</option>
-        </select>
-        <p>Price</p>
-        <input type="text"
-          value={price}
-          name="price"
-          onChange={handleChange}
-        />
-        <p>Info</p>
-        <textarea type="text"
-          value={info}
-          name="info"
-          onChange={handleChange}
-        />
-
-        <div>
-          <button disabled={isFormInvalid()}>
+        <div className="col-4">
+          <p className='form-label'>Item Name</p>
+          <input
+            className='form-control border border-dark'
+            type="text"
+            value={title}
+            name="title"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="col-8">
+          <p className='form-label'>Image Url</p>
+          <input type="text"
+            className='form-control border border-dark'
+            value={imageUrl}
+            name="imageUrl"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="col-4">
+          <p className='form-label'>Item Type:</p>
+          <select name="type"
+            className='form-control border border-dark'
+            value={type}
+            onChange={handleChange}
+          >
+            <option>--Please choose an item type--</option>
+            <option>clothes</option>
+            <option>tumblers</option>
+            <option>misc</option>
+          </select>
+        </div>
+        <div className="col-2">
+          <p className='form-label'>Price</p>
+          <input type="text"
+            className='form-control border border-dark'
+            value={price}
+            name="price"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="col-8 mb-3">
+          <p className='form-label'>Info</p>
+          <textarea type="text"
+            className='form-control border border-dark'
+            value={info}
+            name="info"
+            onChange={handleChange}
+          />
+        </div>
+        <div div className="mb-3">
+          <button className='btn btn-primary border border-dark' disabled={isFormInvalid()}>
             Add Item
           </button>
           <Link to="/">
-            <button>Cancel</button>
+            <button className='btn btn-danger border border-dark'>Cancel</button>
           </Link>
         </div>
       </form>
