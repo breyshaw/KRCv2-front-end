6813aedf56b16f5f72d3f3d3a8d882218296da5a
breyshaw <dawsonbrey@gmail.com>
2022-03-07 16:28:12 -0500

    stub up holiday filter on clothes.jsx

--- src/pages/Clothes/Clothes.jsx
+++ src/pages/Clothes/Clothes.jsx
@@ -79,46 +79,45 @@ const Clothes = (props) => {
       }
 
       {filter === "holiday" ?
-        <h2>Holiday items</h2>
+        <div className="row">
+          {props.items
+            .filter(item => item.subcat === 'holiday')
+            .map(item =>
+              <div key={item._id} className="col-sm-3">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title text-center">{item.title}</h5>
+                    {/* <p class="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
+                    {item.imageUrl ?
+                  <img src={item.imageUrl} className="card-img-bottom" alt=''></img>
+                  : ''
+                }
+                  { item.videoUrl ? 
+                  <video className='card-img-top' autoplay='true'>
+                  <source src={item.videoUrl} type="video/mp4"/>
+                </video>
+                  : ''
+                }
+                <p className="card-text mt-1 mb-0">${item.price}</p>
+                  </div>
+
+                  <Link to='/itemDetails' state={item} className="btn btn-primary">Details</Link>
+
+                  {props.user ?
+                    (props.user.isAdmin) &&
+                    <Link to="/editItem" className="btn btn-warning" state={item}>Edit</Link>
+                    : ''
+                  }
+                  {props.user ?
+                    (props.user.isAdmin) &&
+                    <button onClick={() => props.handleDeleteItem(item._id)} className="btn btn-danger">Delete</button>
+                    : ''
+                  }
+                </div>
+              </div>
+            )}
+        </div>
         : ''
-        // <div className="row">
-        //   {props.items
-        //     .filter(item => item.type === 'clothes')
-        //     .map(item =>
-        //       <div key={item._id} className="col-sm-3">
-        //         <div className="card">
-        //           <div className="card-body">
-        //             <h5 className="card-title text-center">{item.title}</h5>
-        //             {/* <p class="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
-        //             {item.imageUrl ?
-        //           <img src={item.imageUrl} className="card-img-bottom" alt=''></img>
-        //           : ''
-        //         }
-        //           { item.videoUrl ? 
-        //           <video className='card-img-top' autoplay='true'>
-        //           <source src={item.videoUrl} type="video/mp4"/>
-        //         </video>
-        //           : ''
-        //         }
-        //         <p className="card-text mt-1 mb-0">${item.price}</p>
-        //           </div>
-
-        //           <Link to='/itemDetails' state={item} className="btn btn-primary">Details</Link>
-
-        //           {props.user ?
-        //             (props.user.isAdmin) &&
-        //             <Link to="/editItem" className="btn btn-warning" state={item}>Edit</Link>
-        //             : ''
-        //           }
-        //           {props.user ?
-        //             (props.user.isAdmin) &&
-        //             <button onClick={() => props.handleDeleteItem(item._id)} className="btn btn-danger">Delete</button>
-        //             : ''
-        //           }
-        //         </div>
-        //       </div>
-        //     )}
-        // </div>
       }
 
       {filter === "mom" ?
